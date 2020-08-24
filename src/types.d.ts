e726acfd6f477b3ebe7d279c02df303b8e6b05b8
yunhoe <yunhoe@deepnatural.ai>
2020-08-24 23:40:52 +0900

    Useless things are removed and doing works

---
+++ src/types.d.ts
@@ -0,0 +1,16 @@
+/*
+  If you declare this file name as "types.ts", you sholud use 'export' keyword like this:
+
+  export type Todo = {
+    text: string,
+    complete: boolean
+  }
+
+  But if you declare it as "types.d.ts", which mean "declaration ts file", you should not use 'export' keyword like this
+  This process is automatically done by typescript compiler
+*/
+
+type Todo = {
+  text: string,
+  complete: boolean
+}
